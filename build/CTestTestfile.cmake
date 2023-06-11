b0abc296579bb4cb0b05c4a5e695e135bb6d035b
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2023-06-11 19:28:56 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samthedev/Documents/Vulkan
-# Build directory: /home/samthedev/Documents/Vulkan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
