42c452476dd2895a815d2a94bb30fced409cb3ff
SamTheDev <vs.samu.dev@gmail.com>
2023-06-11 19:20:08 +0200

    set up vulkan base

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samthedev/Documents/Vulkan
+# Build directory: /home/samthedev/Documents/Vulkan/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
